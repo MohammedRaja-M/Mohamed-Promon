7e18859f213b0477550fd4db2ab17ed9b837cf47
MohamedRaja-M <mohamed.raja@promon.co.in>
2022-12-29 19:40:28 +0530

    My-Consul

---
+++ Consul-Promon/Spring-Boot-Consul/src/test/java/com/promon/consul/SpringBootConsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promon.consul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
